3f5ced78f5cf263f021579a9c4daf5d6513dcde3
My code <chetannnaik03@gmail.com>
2024-02-11 19:44:25 +0530

    first 2 websites

---
+++ first.js
@@ -0,0 +1,13 @@
+const product = {
+    name:"Ball pen",
+    rating:4,
+    offer: 5,
+    price: 270
+};
+const instagram = {
+    name:"shradhakhapra",
+    posts: 195,
+    followers: 569,
+    following:4,
+};
+console.log(typeof instagram["name"]);
